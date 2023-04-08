c6625ea80fb831866393f750626f836ee1bb2781
Aniket Pradhan <aniketpradhan814@gmail.com>
2023-04-08 07:15:15 +0530

    Frontend starter code added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import Head from "next/head";
+import "@/styles/globals.css";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+  return (
+    <>
+      <Head>
+        <title>Blog App</title>
+        <meta name="description" content="Blog app" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
+
+export default MyApp;
